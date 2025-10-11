33148c794d42c63b1874fef364a5e571b2834088
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-10-11 13:48:22 +0600

    update

--- resources/views/frontend/layouts/partials/header.blade.php
+++ resources/views/frontend/layouts/partials/header.blade.php
@@ -5,8 +5,16 @@
                 <h1 class="text-2xl font-bold text-white">B33 Rentals</h1>
             </a>
             <nav class="flex items-center justify-end gap-4">
-                <a href="{{ route('login') }}" class="text-white" wire:navigate>Login</a>
-                <a href="{{ route('register') }}" class="text-white" wire:navigate>Register</a>
+                @auth
+                    @if (auth()->guard('web')->check())
+                        <a href="{{ route('user.profile') }}" class="text-white" wire:navigate>Profile</a>
+                    @else
+                        <a href="{{ route('admin.dashboard') }}" class="text-white" wire:navigate>Dashboard</a>
+                    @endif
+                @else
+                    <a href="{{ route('login') }}" class="text-white" wire:navigate>Login</a>
+                    <a href="{{ route('register') }}" class="text-white" wire:navigate>Register</a>
+                @endauth
             </nav>
         </div>
     </div>
