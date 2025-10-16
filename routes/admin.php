59af5d2565e72f06d5de27dacfbfd62fa4a0de6b
dev-sumon <akhtaruzzamansumon7@gmail.com>
2025-10-16 15:11:34 +0600

    Revert "email verify done"
    This reverts commit 8417639f50995288c7e8cb9aa9db60984e6df813.

--- routes/admin.php
+++ routes/admin.php
@@ -6,7 +6,6 @@ use App\Livewire\Backend\Admin\Components\UserManagement\AllUser;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:admin', 'admin'])->name('admin.')->prefix('admin')->group(function () {
-    
     Route::get('/dashboard', function () {
         return view('backend.admin.pages.dashboard');
     })->name('dashboard');
