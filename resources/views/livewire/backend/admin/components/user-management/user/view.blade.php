a05cb94aa79867acfd23fce99fa911b301e1e930
dev-sumon <akhtaruzzamansumon7@gmail.com>
2025-10-17 10:16:10 +0600

    user view page design update

--- resources/views/livewire/backend/admin/components/user-management/user/view.blade.php
+++ resources/views/livewire/backend/admin/components/user-management/user/view.blade.php
@@ -1,4 +1,4 @@
-<div>
+{{-- <div>
     <p><strong>Name:</strong> {{ $user->name }}</p>
     <p><strong>Email:</strong> {{ $user->email }}</p>
     <p><strong>Phone:</strong> {{ $user->phone ?? 'N/A' }}</p>
