85a19f92db52449c6417d1f3ad0af1a1009717fc
Md Monirul Islam Taluqdar <82707883+monirulislam0@users.noreply.github.com>
2025-10-19 10:43:42 +0600

    Update admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,7 @@
 <?php
 
 use App\Http\Controllers\Backend\Admin\AdminManagement\AdminController;
-use App\Livewire\Backend\Admin\Components\Dashboard;
-use App\Livewire\Backend\Admin\Components\UserManagement\AllUser;
+
 use App\Http\Controllers\Backend\Admin\GameManagement\CategoryController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,16 +20,7 @@ Route::middleware(['auth:admin', 'admin', 'adminVerify'])->name('admin.')->prefi
         });
     });
 
-    Route::group(['prefix' => 'game-management', 'as' => 'gm.'], function () {
-        Route::controller(CategoryController::class)->name('category.')->prefix('category')->group(function () {
-             Route::get('/', 'index')->name('index');
-             Route::get('/create', 'create')->name('create');
-            Route::get('/edit/{id}', 'edit')->name('edit');
-            // Route::get('/view/{id}', 'show')->name('view');
-            // Route::get('/trash', 'trash')->name('trash');
-            
-        });
-    });
+
 
     Route::group(['prefix' => 'game-management', 'as' => 'gm.'], function () {
         Route::controller(CategoryController::class)->name('category.')->prefix('category')->group(function () {
