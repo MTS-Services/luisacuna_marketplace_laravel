238d2696ba5b2b4c8a102e1a0a6b63e3f47c0a50
dev-sumon <akhtaruzzamansumon7@gmail.com>
2025-10-25 16:38:01 +0600

    UserReferral and UserStatistic model query, Accessors, Methods update

--- app/Models/UserReferral.php
+++ app/Models/UserReferral.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserReferral extends BaseModel
 {
-       protected $fillable = [
+    protected $fillable = [
         'sort_order',
         'user_id',
         'referral_code',
@@ -50,8 +50,45 @@ class UserReferral extends BaseModel
     }
     public function currency_id()
     {
-        return $this->belongsTo(Currency::class,'currency_id', 'id');
+        return $this->belongsTo(Currency::class, 'currency_id', 'id');
     }
 
 
+    /*
+    |--------------------------------------------------------------------------
+    | Query Scopes
+    |--------------------------------------------------------------------------
+    */
+
+    public function scopeSearch($query, $search)
+    {
+        return $query->where(function ($q) use ($search) {
+            $q->where('referral_code', 'like', "%{$search}%");
+        });
+    }
+    public function scopeFilder($query, array $filters)
+    {
+        $query->when($filters['search'] ?? null, fn($q, $search) => $q->search($search));
+        $query->when($filters['status'] ?? null, fn($q, $status) => $q->where('is_active', $status));
+        return $query;
+    }
+
+
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Accessors
+    |--------------------------------------------------------------------------
+    */
+
+
+
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Methods
+    |--------------------------------------------------------------------------
+    */
 }
--- app/Models/UserStatistic.php
+++ app/Models/UserStatistic.php
@@ -6,7 +6,7 @@ use App\Models\BaseModel;
 
 class UserStatistic extends BaseModel
 {
-      protected $fillable = [
+    protected $fillable = [
         'sort_order',
         'user_id',
         'total_orders_as_buyer',
@@ -43,4 +43,49 @@ class UserStatistic extends BaseModel
     {
         return $this->belongsTo(Currency::class);
     }
+
+
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Query Scopes
+    |--------------------------------------------------------------------------
+    */
+
+
+    // public function scopeSearch($query, $search)
+    // {
+    //     return $query->where(function ($q) use ($search) {
+    //         $q->where('title', 'like', "%{$search}%")
+    //             ->orWhere('description', 'like', "%{$search}%");
+    //     });
+    // }
+
+    public function scopeFilder($query, array $filters)
+    {
+        $query->when($filters['search'] ?? null, fn($q, $search) => $q->search($search));
+        $query->when($filters['status'] ?? null, fn($q, $status) => $q->where('is_active', $status))
+            ->when($filters['user_id'] ?? null, fn($q, $user_id) => $q->where('user_id', $user_id))
+            ->when($filters['currency_id'] ?? null, fn($q, $currency_id) => $q->where('currency_id', $currency_id));
+        return $query;
+    }
+
+
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Accessors
+    |--------------------------------------------------------------------------
+    */
+
+
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Methods
+    |--------------------------------------------------------------------------
+    */
 }
