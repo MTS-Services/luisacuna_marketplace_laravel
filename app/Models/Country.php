879f348106f54f4fe099254da7061c29f555dd8a
dev-sumon <akhtaruzzamansumon7@gmail.com>
2025-10-25 16:32:06 +0600

    Country model scope update

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -41,4 +41,50 @@ class Country extends BaseModel
     /* =#=#=#=#=#=#=#=#=#=#==#=#=#=#= =#=#=#=#=#=#=#=#=#=#==#=#=#=#=
                 End of RELATIONSHIPS
      =#=#=#=#=#=#=#=#=#=#==#=#=#=#= =#=#=#=#=#=#=#=#=#=#==#=#=#=#= */
+
+    /*
+    |--------------------------------------------------------------------------
+    | Query Scopes
+    |--------------------------------------------------------------------------
+    */
+
+    public function scopeActive($query)
+    {
+        return $query->where('is_active', true);
+    }
+    public function scopeInactive($query)
+    {
+        return $query->where('is_active', false);
+    }
+    public function scopeSearch($query, $search)
+    {
+        return $query->where(function ($q) use ($search) {
+            $q->where('name', 'like', "%{$search}%")
+                ->orWhere('code', 'like', "%{$search}%")
+                ->orWhere('phone_code', 'like', "%{$search}%")
+                ->orWhere('currency', 'like', "%{$search}%");
+        });
+    }
+    public function scopeFilder($query, array $filters)
+    {
+        $query->when($filters['search'] ?? null, fn($q, $search) => $q->search($search));
+        $query->when($filters['status'] ?? null, fn($q, $status) => $q->where('is_active', $status));
+        return $query;
+    }
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Accessors
+    |--------------------------------------------------------------------------
+    */
+
+
+
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Methods
+    |--------------------------------------------------------------------------
+    */
 }
