7a5ef113ca97692e5125c5a5f86e5827e371a6d3
dev-sumon <akhtaruzzamansumon7@gmail.com>
2025-10-27 11:56:18 +0600

    profile design done

---
+++ resources/views/livewire/frontend/components/user-account.blade.php
@@ -0,0 +1,49 @@
+<main class=" mx-auto relative">
+    <section class="">
+        <div class="inner_banner">
+            <img src="{{ asset('assets/images/user_profile/inner_banner.png') }}" alt="" class="w-full">
+        </div>
+    </section>
+    {{-- profile header --}}
+    <section class="container mx-auto bg-zinc-900 p-10! rounded-2xl absolute top-20 left-1/2 translate-x-[-50%] ">
+        <div class="flex justify-between">
+            <div class="flex items-center gap-6">
+                <div class="">
+                    <img src="{{ asset('assets/images/user_profile/Ellipse 474.png') }}" alt="">
+                </div>
+                <div class="">
+                    <h3 class="text-4xl font-semibold mb-2">Starriz.clo</h3>
+                    <span class="text-base-400">Online</span>
+                </div>
+            </div>
+            <div class="icon">
+                <x-flux::icon name="pencil-square" class="w-6 h-6 inline-block" stroke="white" />
+            </div>
+        </div>
+        <div class="border-b border-zinc-700 mt-6 mb-4"></div>
+        <div class="flex gap-6">
+            <a href="#" class=" group">
+                <span class="relative z-10 ">Shop</span>
+                <span class=""></span>
+            </a>
+            <a href="#" class=" group">
+                <span class="relative z-10">Reviews</span>
+                <span class=""></span>
+            </a>
+            <a href="#" class=" group">
+                <span class="relative z-10">About</span>
+                <span class=""></span>
+            </a>
+        </div>
+    </section>
+    <div class="min-h-70"></div>
+    {{-- info --}}
+    <section class="container mx-auto mb-30">
+        <div class="mb-6">
+            <h3 class="text-4xl text-zinc-50 mb-4">Shop</h3>
+            {{-- profile nav --}}
+            @include('components.frontend.profile-nav')
+            {{-- select game --}}
+            
+    </section>
+</main>
--- routes/frontend.php
+++ routes/frontend.php
@@ -5,9 +5,11 @@ use App\Livewire\Frontend\Buttons;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\Frontend\CurrencyController;
+use App\Http\Controllers\Frontend\UserAccountController;
 use App\Http\Controllers\Frontend\UserProfileController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/currency', [CurrencyController::class, 'index'])->name('currency');
 Route::get('profile', [UserProfileController::class, 'profile'])->name('profile');
+Route::get('account', [UserAccountController::class, 'account'])->name('account');
