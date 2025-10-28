25d537bb9d1e53c6c3aab4a71f3172cbadf9c482
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-10-28 15:19:49 +0600

    Update header.blade.php

--- resources/views/backend/admin/layouts/partials/header.blade.php
+++ resources/views/backend/admin/layouts/partials/header.blade.php
@@ -70,7 +70,7 @@
                     <div x-show="open" @click.away="open = false" x-transition:enter="transition ease-out duration-200"
                         x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100"
                         x-transition:leave="transition ease-in duration-75"
-                        class="hidden absolute right-0 mt-2 w-fit min-w-40 glass-card bg-bg-secondary rounded-xl shadow-lg py-2 z-50"
+                        class="hidden absolute right-0 mt-2 w-fit min-w-40 glass-card bg-bg-primary rounded-xl shadow-lg py-2 z-50"
                         :class="open ? '!block' : '!hidden'">
 
                         <div class="px-4 py-2 border-b border-white/10">
