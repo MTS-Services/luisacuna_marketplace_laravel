5df808e03799a272c3377bf19a566fdf6579d0c6
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-10-28 14:43:26 +0600

    Update input.blade.php

--- resources/views/components/ui/input.blade.php
+++ resources/views/components/ui/input.blade.php
@@ -29,6 +29,6 @@
 @else
     <input type="{{ $type }}" {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
         'class' =>
-            'w-full shadow-sm px-3 py-2 rounded-md border border-zinc-300 focus:border-accent focus:ring-accent focus:ring-1 disabled:opacity-50 disabled:cursor-not-allowed transition duration-150 bg-white text-zinc-900 placeholder-zinc-400 focus:outline-none focus:ring-offset-0',
+            'w-full shadow-sm px-3 py-2 bg-transparent! dark:text-zinc-100! text-zinc-900  rounded-md border border-zinc-300 focus:border-accent focus:ring-accent focus:ring-1 disabled:opacity-50 disabled:cursor-not-allowed transition duration-150 bg-white text-zinc-900 placeholder-zinc-400 focus:outline-none focus:ring-offset-0',
     ]) !!}>
 @endif
