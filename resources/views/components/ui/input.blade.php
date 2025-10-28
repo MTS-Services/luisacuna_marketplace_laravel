839f4108c3e2dafddeab2f26ab7c0346efab0c65
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-10-28 15:17:02 +0600

    Update input.blade.php

--- resources/views/components/ui/input.blade.php
+++ resources/views/components/ui/input.blade.php
@@ -14,7 +14,7 @@
             :class="{ 'pe-10': !isRtl, 'ps-10': isRtl }"
             {!! $attributes->merge([
                 'class' =>
-                    'w-full shadow-sm px-3 py-2 rounded-md border border-zinc-300 focus:border-accent focus:ring-accent focus:ring-1 disabled:opacity-50 disabled:cursor-not-allowed transition duration-150 bg-white text-zinc-900 placeholder-zinc-400 focus:outline-none focus:ring-offset-0',
+                    'w-full shadow-sm px-3 py-2 bg-transparent! dark:text-zinc-100! text-zinc-900  rounded-md border border-zinc-300 focus:border-accent focus:ring-accent focus:ring-1 disabled:opacity-50 disabled:cursor-not-allowed transition duration-150 bg-white text-zinc-900 placeholder-zinc-400 focus:outline-none focus:ring-offset-0',
             ]) !!}>
 
         <span class="absolute top-1/2 transform -translate-y-1/2 cursor-pointer text-gray-400 dark:text-gray-500
