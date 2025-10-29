c3e9dda9977a7c34e24fe4d5c1e8066f51b22247
Md Shariful Islam <116838065+devshariful99@users.noreply.github.com>
2025-10-29 12:07:07 +0600

    Update View.php

--- app/Livewire/Backend/Admin/AuditLogManagement/View.php
+++ app/Livewire/Backend/Admin/AuditLogManagement/View.php
@@ -2,12 +2,20 @@
 
 namespace App\Livewire\Backend\Admin\AuditLogManagement;
 
+use App\Models\Audit;
 use Livewire\Component;
 
 class View extends Component
 {
+    public Audit $data;
+    public function mount(Audit $data): void
+    {
+        $this->data = $data;
+    }
     public function render()
     {
-        return view('livewire.backend.admin.audit-log-management.view');
+        return view('livewire.backend.admin.audit-log-management.view', [
+            'data' => $this->data,
+        ]);
     }
 }
