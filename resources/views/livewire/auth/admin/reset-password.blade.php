8fd711aa074473bd1147b40aeeac3fe6de1d5fa8
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-10-29 19:08:15 +0600

    reset page design done

--- resources/views/livewire/auth/admin/reset-password.blade.php
+++ resources/views/livewire/auth/admin/reset-password.blade.php
@@ -8,21 +8,21 @@
                 }
             </script>
 
-            <div class="flex items-center justify-center  text-white px-4 sm:px-6 lg:px-8">
+            <div class="flex items-center justify-center  text-text-white px-4 sm:px-6 lg:px-8">
                 <form method="POST" wire:submit.prevent="resetPassword" class="w-full max-w-md sm:max-w-lg md:max-w-xl">
                     <div class="bg-bg-primary rounded-2xl p-6 sm:p-8 shadow-lg flex flex-col justify-between min-h-[60vh] ">
 
                         <!-- Header -->
                         <div class="mb-6 text-center">
-                            <h2 class="text-3xl sm:text-4xl font-medium text-white">Create Password</h2>
-                            <p class="text-gray-300 lg:text-xl sm:text-lg mt-2">
+                            <h2 class="text-3xl sm:text-4xl font-medium text-text-white">Create Password</h2>
+                            <p class="text-text-white lg:text-xl sm:text-lg mt-2">
                                 Hi! Welcome back, you’ve been missed
                             </p>
                         </div>
 
                         <!-- Email -->
                         <div class="mb-4 sm:mb-6 px-2 sm:px-6">
-                            <label class="block text-lg sm:text-2xl font-medium mb-2 text-white">Email</label>
+                            <label class="block text-lg sm:text-2xl font-medium mb-2 text-text-white">Email</label>
                             <x-ui.input type="email" placeholder="example@gmail.com" disabled wire:model="email"
                                />
                         </div>
@@ -33,14 +33,14 @@
 
                         <!-- Password -->
                         <div class="mb-2 sm:mb-6 px-2 sm:px-6">
-                            <label class="block text-lg sm:text-2xl font-medium mb-2 text-white">Password</label>
+                            <label class="block text-lg sm:text-2xl font-medium mb-2 text-text-white">Password</label>
 
                             <div class="relative">
                                 <x-ui.input type="password" id="password" placeholder="Aex@8465" wire:model="password"
                                      />
                                 <button type="button" onclick="togglePassword('password')"
-                                    class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-300">
-                                    <svg class="w-5 h-5" fill="none" stroke="gray" viewBox="0 0 24 24">
+                                    class="absolute right-3 top-1/2 transform -translate-y-1/2 text-zinc-400 hover:text-zinc-300">
+                                    <svg class="w-5 h-5" fill="none" stroke="zinc" viewBox="0 0 24 24">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                             d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -57,14 +57,14 @@
 
                         <!-- Confirm Password -->
                         <div class="mb-2 sm:mb-6 px-2 sm:px-6">
-                            <label class="block text-lg sm:text-2xl font-medium mb-2 text-white">Confirm
+                            <label class="block text-lg sm:text-2xl font-medium mb-2 text-text-white">Confirm
                                 Password</label>
 
                             <div class="relative">
                                 <x-ui.input type="password" id="confirmPassword" placeholder="Aex@8465"
                                     wire:model="password_confirmation"/>
                                 <button type="button" onclick="togglePassword('confirmPassword')"
-                                    class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-300">
+                                    class="absolute right-3 top-1/2 transform -translate-y-1/2 text-zinc-400 hover:text-zinc-300">
                                     <svg class="w-5 h-5" fill="none" stroke="gray" viewBox="0 0 24 24">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                             d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
@@ -83,7 +83,7 @@
                         <!-- Submit button -->
                         <div class="flex justify-center mb-6 px-2 sm:px-6">
                             <button type="submit"
-                                class="bg-zinc-600 hover:bg-zinc-700 transition-colors text-white font-medium py-3 w-full sm:w-auto sm:px-24 md:px-48 rounded-full">
+                                class="bg-zinc-600 hover:bg-zinc-700 transition-colors text-text-white font-medium py-3 w-full sm:w-auto sm:px-24 md:px-48 rounded-full">
                                 Sign in
                             </button>
                         </div>
--- resources/views/livewire/auth/user/reset-password.blade.php
+++ resources/views/livewire/auth/user/reset-password.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col gap-6">
     <div class="flex flex-col gap-6">
-        <div class="container mx-auto min-h-[80vh]">
+        <div class="container mx-auto min-h-[80vh] py-10 sm:py-15">
             <script>
                 function togglePassword(id) {
                     const input = document.getElementById(id);
@@ -8,23 +8,23 @@
                 }
             </script>
 
-            <div class="min-h-screen flex items-center justify-center bg-[#0D061A] text-white px-4 sm:px-6 lg:px-8">
+            <div class="flex items-center justify-center px-4 sm:px-6 lg:px-8">
                 <form method="POST" wire:submit.prevent="resetPassword" class="w-full max-w-md sm:max-w-lg md:max-w-xl">
-                    <div class="bg-[#1a0b2e] rounded-2xl p-6 sm:p-8 shadow-lg flex flex-col justify-between h-[900px]">
+                    <div class="bg-bg-primary rounded-2xl p-6 sm:p-8 shadow-lg flex flex-col justify-between min-h-[60vh]">
 
                         <!-- Header -->
                         <div class="mb-6 text-center">
-                            <h2 class="text-3xl sm:text-4xl font-medium text-white">Create Password</h2>
-                            <p class="text-gray-300 lg:text-xl sm:text-lg mt-2">
+                            <h2 class="text-3xl sm:text-4xl font-medium text-text-white">Create Password</h2>
+                            <p class="text-text-whitelg:text-xl sm:text-lg mt-2">
                                 Hi! Welcome back, you’ve been missed
                             </p>
                         </div>
 
                         <!-- Email -->
                         <div class="mb-4 sm:mb-6 px-2 sm:px-6">
-                            <label class="block text-lg sm:text-2xl font-medium mb-2 text-white">Email</label>
-                            <input type="email" placeholder="example@gmail.com" wire:model="email"
-                                class="text-white w-full px-4 py-2 bg-[#2d1f43] border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 placeholder-gray-400" />
+                            <label class="block text-lg sm:text-2xl font-medium mb-2 text-text-white">Email</label>
+                            <x-ui.input type="email" placeholder="example@gmail.com" wire:model="email"
+                            />
                         </div>
 
                         @error('email')
@@ -33,13 +33,13 @@
 
                         <!-- Password -->
                         <div class="mb-2 sm:mb-6 px-2 sm:px-6">
-                            <label class="block text-lg sm:text-2xl font-medium mb-2 text-white">Password</label>
+                            <label class="block text-lg sm:text-2xl font-medium mb-2 text-text-white">Password</label>
 
                             <div class="relative">
-                                <input type="password" id="password" placeholder="Aex@8465" wire:model="password"
-                                    class="text-white w-full px-4 py-2 bg-[#2d1f43] border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 placeholder-gray-400" />
+                                <x-ui.input type="password" id="password" placeholder="Aex@8465" wire:model="password"
+                                />
                                 <button type="button" onclick="togglePassword('password')"
-                                    class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-300">
+                                    class="absolute right-3 top-1/2 transform -translate-y-1/2 text-zinc-400 hover:text-zinc-300">
                                     <svg class="w-5 h-5" fill="none" stroke="gray" viewBox="0 0 24 24">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                             d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
@@ -58,15 +58,14 @@
 
                         <!-- Confirm Password -->
                         <div class="mb-2 sm:mb-6 px-2 sm:px-6">
-                            <label class="block text-lg sm:text-2xl font-medium mb-2 text-white">Confirm
+                            <label class="block text-lg sm:text-2xl font-medium mb-2 text-text-white">Confirm
                                 Password</label>
 
                             <div class="relative">
-                                <input type="password" id="confirmPassword" placeholder="Aex@8465"
-                                    wire:model="password_confirmation"
-                                    class="text-white w-full px-4 py-2 bg-[#2d1f43] border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 placeholder-gray-400" />
+                                <x-ui.input type="password" id="confirmPassword" placeholder="Aex@8465"
+                                    wire:model="password_confirmation"/>
                                 <button type="button" onclick="togglePassword('confirmPassword')"
-                                    class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-300">
+                                    class="absolute right-3 top-1/2 transform -translate-y-1/2 text-zinc-400 hover:text-zinc-300">
                                     <svg class="w-5 h-5" fill="none" stroke="gray" viewBox="0 0 24 24">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                             d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
@@ -80,16 +79,16 @@
 
                         <!-- Submit button -->
                         <div class="flex justify-center mb-6 px-2 sm:px-6">
-                            <button  type="submit"
-                                class="bg-[#853fee] hover:bg-purple-700 transition-colors text-white font-medium py-3 w-full sm:w-auto sm:px-24 md:px-48 rounded-full">
+                            <x-ui.button  type="submit"
+                                class="w-auto py-2!">
                                 Sign in 
-                            </button>
+                            </x-ui.button>
                         </div>
 
                         <!-- Divider -->
                         <div class="flex items-center mb-6 px-4">
                             <hr class="flex-1 border-gray-700" />
-                            <span class="px-3 text-sm sm:text-md text-gray-200">Or sign in with</span>
+                            <span class="px-3 text-sm sm:text-md text-zinc-200">Or sign in with</span>
                             <hr class="flex-1 border-gray-700" />
                         </div>
 
@@ -115,9 +114,9 @@
                         </div>
 
                         <!-- Sign up link -->
-                        <div class="text-center text-sm text-gray-300">
+                        <div class="text-center text-sm text-text-white">
                             Don’t have an account?
-                            <a href="{{ route('register') }}" class="text-purple-400 hover:underline">Sign up</a>
+                            <a href="{{ route('register') }}" class="text-purple-400 hover:underline" wire:navigate>Sign up</a>
                         </div>
                     </div>
                 </form>
