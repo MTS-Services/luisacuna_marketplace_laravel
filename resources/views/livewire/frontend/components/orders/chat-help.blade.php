e057b303959ec265412315c1bd65ca8b9c0cc1c9
Md Monirul Islam Taluqdar <82707883+monirulislam0@users.noreply.github.com>
2025-10-29 18:35:12 +0600

    Test

--- resources/views/livewire/frontend/components/orders/chat-help.blade.php
+++ resources/views/livewire/frontend/components/orders/chat-help.blade.php
@@ -49,7 +49,7 @@
                             </div>
                         </div>
                         <div>
-                           
+
                             <div class="flex justify-between pb-3">
                                 <p class="text-base">Username</p>
                                 <p class="flex flex-end">
@@ -86,10 +86,11 @@
                     <div class="item-list mt-10">
                         <div class="w-1/3 flex flex-col">
                             <div class="p-2 w-100 h-100">
-                                <img src="{{ asset('assets/images/orders/Mercury_LMG_-_Schematic_-_Save_the_World .png') }}" alt="">
+                                <img src="{{ asset('assets/images/orders/Mercury_LMG_-_Schematic_-_Save_the_World .png') }}"
+                                    alt="">
                             </div>
                             <div class="pl-2">
-                                <p class="mt-6 font-semibold text-xl"> {{ __('Description')}}</p>
+                                <p class="mt-6 font-semibold text-xl"> {{ __('Description') }}</p>
                                 <p class="mt-3 text-base">Mercury Spark - Ultimate 5-start Boost</p>
                             </div>
                         </div>
@@ -97,81 +98,22 @@
                 </div>
             </div>
 
-            <div class="additional-info pb-10">
-                <div class="w-full bg-bg-base rounded-3xl p-20">
-                    <div class="item-title pb-10">
-                        <div class="item-short-info flex justify-between">
-                            <div class="flex flex-nowrap">
-                                <div class="item-image">
-                                    <img src="{{ asset('assets/images/orders/Rectangle 12420.png') }}"
-                                        alt="Mercury Spark — Ultimate 5-Star Boost" class="h-10 w-10">
-                                </div>
-                                <div class="item-title pl-5">
-                                    <h3 class="text-primary-50 text-2xl ">Mercury Spark — Ultimate 5-Star Boost</h3>
+            <div class="payment-info w-full p-20  bg-white/6  rounded-3xl mb-10">
 
-                                </div>
+                <div class="item-title pb-5">
+                    <div class="item-short-info flex justify-between">
+                        <div class="flex flex-nowrap">
+                            
+                            <div class="item-title ">
+
+                                <h3 class="text-primary-50 text-2xl ">{{ __('Seller') }}</h3>
+
                             </div>
-
-                        </div>
-                    </div>
-            </div>
-        </div>
-
-
-        <div class="chat mt-10 mb-16 px-20 py-10 bg-white/10 rounded-3xl">
-            <div class="mb-10">
-                <div class="py-7 px-7 rounded-3xl bg-bg-base flex flex-row border-l-3 border-pink-500">
-                    <div class="relative mr-5">
-                        <img src="{{ asset('/assets/images/orders/2 2.png') }}" class="w-13.5 h-13.5" alt="">
-                        <span class="h-2 w-2 bg-blue-500 absolute rounded-full right-0 bottom-2.5"></span>
-                    </div>
-                    <div>
-                        <p class="text-xl font-semibold">{{ __('D18QUAN') }}</p>
-                        <p class="text-base">{{ __('Order for this') }}</p>
-                    </div>
-                </div>
-            </div>
-            <div class="w-full mb-10">
-                <div class="py-7 px-7 rounded-3xl bg-bg-base flex flex-col border-l-4 border-pink-500 w-full">
-                    <div class="flex flex-col w-full ">
-                        <p class="text-text-primary break-all">
-                            {{ __('Order Created: https//www_companyname_gWorder/98bc4674-4bde-4498-9175-a4a0318458eO') }}
-                        </p>
-
-                        <div class="mt-3 w-full  bg-white/5 p-4 rounded-xl">
-                            <p class="flex items-center">
-                                <flux:icon name="link" class="inline-block pr-2" />
-                                {{ __('98bc46744bde-4498-9175-a4a031U58eO') }}
-                            </p>
-                            <sub class="text-gray-500">
-                                {{ __('www.companyname.com') }}
-                            </sub>
                         </div>
 
-                        <p class="mt-3 text-primary-50 text-right">{{ __('Oct-20-2025') }}</p>
                     </div>
                 </div>
-            </div>
-            <div class="w-full mb-10">
-                <div class="py-7 px-7 rounded-3xl bg-bg-base flex flex-col border-l-4 border-pink-500 w-full">
-                    <div class="flex flex-col w-full ">
-                        <div class="mt-3 w-full  bg-white/5 p-4 rounded-xl">
-                            <p class="flex items-center">
-                                <flux:icon name="link" class="inline-block pr-2" />
-                                {{ __('98bc46744bde-4498-9175-a4a031U58eO') }}
-                            </p>
-                            <sub class="text-gray-500">
-                                {{ __('www.companyname.com') }}
-                            </sub>
-                        </div>
-
-                        <p class="mt-3 text-primary-50 text-right">{{ __('Oct-20-2025') }}</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="mb-10">
-                <div class="py-7  rounded-3xl flex flex-row items-start">
+                <div class="  rounded-3xl flex flex-row items-start">
                     <div>
                         <div class="relative mr-5">
                             <img src="{{ asset('/assets/images/orders/2 2.png') }}" class="w-13.5 h-13.5"
@@ -181,98 +123,79 @@
                     </div>
                     <div class="w-8/9">
                         <div class="flex flex-col w-full ">
-                            <div class=" w-full  bg-white/5 p-4 rounded-xl">
+                            <div class=" w-full ">
                                 <p class="flex items-center">
-
-                                    {{ __('98bc46744bde-4498-9175-a4a031U58eO') }}
+                                    {{ __('Sohan') }}
+                                </p>
+                                <p class="pt-2">
+                                    <span class="pr-2 text-xs font-normal ">Like |</span>
+                                    <span class="pr-2 text-xs font-normal ">2434 Reviews |</span>
+                                    <span class="text-xs font-normal">1642 Sold</span>
                                 </p>
                             </div>
-
-                            <p class="mt-3 text-primary-50 text-right">{{ __('Oct-20-2025') }}</p>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div class="mb-10">
-                <div class="py-7  rounded-3xl flex flex-row-reverse items-start">
-                    <div>
-                        <div class="relative ml-5">
-                            <img src="{{ asset('/assets/images/orders/2 2.png') }}" class="w-13.5 h-13.5"
-                                alt="">
-                            <span class="h-2 w-2 bg-blue-500 absolute rounded-full right-0 bottom-2.5"></span>
-                        </div>
-                    </div>
-                    <div class="w-8/9">
-                        <div class="flex flex-col w-full ">
-                            <div class=" w-full  bg-white/5 p-4 rounded-xl">
-                                <p class="flex items-center">
+            <div class="payment-info w-full p-20  bg-white/6  rounded-3xl  mb-25">
 
-                                    {{ __('98bc46744bde-4498-9175-a4a031U58eO') }}
-                                </p>
+                <div class="item-title pb-10">
+                    <div class="item-short-info flex justify-between">
+                        <div class="flex flex-nowrap">
+                            
+                            <div class="item-title ">
+
+                                <h3 class="text-primary-50 text-2xl ">{{ __('Payment') }}</h3>
+
                             </div>
-
-                            <p class="mt-3 text-primary-50 text-left">{{ __('Oct-20-2025') }}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="w-full mb-10">
-                <div class="py-7 px-7 rounded-3xl bg-bg-base flex flex-col border-l-4 border-pink-500 w-full">
-                    <div class="flex flex-col w-full ">
-                        <div class="mt-3 w-full  bg-white/5 p-4 rounded-xl">
-                            <p class="flex items-center">
-                                <flux:icon name="link" class="inline-block pr-2" />
-                                {{ __('98bc46744bde-4498-9175-a4a031U58eO') }}
-                            </p>
-                            <sub class="text-gray-500">
-                                {{ __('www.companyname.com') }}
-                            </sub>
-                        </div>
-
-                        <p class="mt-3 text-primary-50 text-right">{{ __('Oct-20-2025') }}</p>
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="">
-                <div class="py-7  rounded-3xl ">
-
-                    <div class="flex flex-col w-full ">
-                        <div class=" w-full  bg-white/5 p-4 rounded-xl">
-                            <p class="flex items-center">
-
-                                {{ __('this conversation is no longer active. ') }}
-                            </p>
                         </div>
 
                     </div>
                 </div>
-            </div>
-        </div>
+                <div class="w-full ">
 
-
-        <div class="secruity-tips pb-25">
-            <div class="w-full mb-10">
-                <div class="py-7 px-7 rounded-3xl bg-bg-base flex flex-col w-full">
-                    <div class="flex flex-col w-full ">
-                        <div class="mt-3 w-full ">
-                            <p class="flex items-center text-3xl text-primary-50 font-semibold">
-                                <img src="{{ asset('assets/images/icons/Rectangle 12420.png') }}" alt=""
-                                    class="w-17 h-17">
-                                {{ __('Security tips') }}
-                            </p>
-                        </div>
-                        <p class="text-base text-primary-50 regular ">
-                            {{ __('For your protection, you must always pay and communicate directly through the Eldorado website. If you stay on Eldorado throughout the entire transaction—from payment, to communication, to delivery—you are protected by Tradeshield. Additionally, be aware that sellers will never ask for your currency or items back after the order is delivered, so you should not respond to any messages requesting this.') }}
+                    <div class="flex justify-between pb-3">
+                        <p class="text-base">Total Price</p>
+                        <p class="flex flex-end">
+                            <span class="text-base pl-3"> $ <span> {{ __('1.20') }}</span></span>
                         </p>
                     </div>
-
+                    <div class="flex justify-between pb-3">
+                        <p class="text-base">Payment Fee </p>
+                        <p class="flex flex-end">
+                            <span class="text-base pl-3">0.00</span>
+                        </p>
+                    </div>
+                    <div class="flex justify-between pb-3">
+                        <p class="text-base">Quantity</p>
+                        <p class="flex flex-end">
+                            <span class="text-base pl-3">6</span>
+                        </p>
+                    </div>
+                    <div class="flex justify-between pb-3">
+                        <p class="text-base">Guaranteed Delivery time</p>
+                        <p class="flex flex-end">
+                            <span class="text-base pl-3"> 1 Days </span>
+                        </p>
+                    </div>
+                    <div class="flex justify-between pb-3">
+                        <p class="text-base">Company Balance</p>
+                        <p class="flex flex-end">
+                            <span class="text-base pl-3"> in-game Delivery </span>
+                        </p>
+                    </div>
+                    <div class="flex justify-between pb-3 border-t-2 border-zinc-500">
+                        <p class="text-base">Total:</p>
+                        <p class="flex flex-end">
+                            <span class="text-base pl-3"> $ 0.00 </span>
+                        </p>
+                    </div>
                 </div>
             </div>
+
+
+
+
         </div>
-    </div>
 </section>
