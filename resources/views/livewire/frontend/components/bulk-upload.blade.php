9c1e6f7ab45f876badc638bd841320d0367ae5ec
mdazizulrahman2580-sys <mdazizulrahman2580@gmail.com>
2025-11-01 14:15:44 +0600

    bulk-update

--- resources/views/livewire/frontend/components/bulk-upload.blade.php
+++ resources/views/livewire/frontend/components/bulk-upload.blade.php
@@ -9,7 +9,7 @@
 
         <a href="#" class="block bg-zinc-800 hover:bg-zinc-700 transition duration-200 rounded-xl p-6 flex  items-center justify-between shadow-lg group">
             <div class="flex items-center space-x-4 ">
-                <div class="bg-zinc-800 p-3 rounded-lg text-text-white group-hover:bg-zinc-700 transition duration-200  ">
+                <div class="bg-zinc-700 p-3 rounded-lg text-text-white group-hover:bg-zinc-700  transition duration-200  ">
                  <svg xmlns="http://www.w3.org/2000/svg"  width="44" height="44" fill="#fff" viewBox="0 0 256 256"><path d="M209.66,122.34a8,8,0,0,1,0,11.32l-82.05,82a56,56,0,0,1-79.2-79.21L147.67,35.73a40,40,0,1,1,56.61,56.55L105,193A24,24,0,1,1,71,159L154.3,
                     74.38A8,8,0,1,1,165.7,85.6L82.39,170.31a8,8,0,1,0,11.27,11.36L192.93,81A24,24,0,1,0,159,47L59.76,147.68a40,40,0,1,0,56.53,56.62l82.06-82A8,8,0,0,1,209.66,122.34Z"></path></svg>
                 </div>
@@ -27,7 +27,7 @@
 
         <a href="#" class="block bg-zinc-800 hover:bg-zinc-700 transition duration-200 rounded-xl p-6 flex items-center justify-between shadow-lg group">
             <div class="flex items-center space-x-4">
-                <div class="bg-zinc-800 p-3 rounded-lg   text-text-white group-hover:bg-zinc-700 transition duration-200">
+                <div class="bg-zinc-700 p-3 rounded-lg   text-text-white group-hover:bg-zinc-700 transition duration-200">
                     <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" fill="#fff" viewBox="0 0 256 256"><path d="M104,40H56A16,16,0,0,0,40,56v48a16,16,0,0,0,16,16h48a16,16,0,0,0,16-16V56A16,16,0,0,0,104,40Zm0,64H56V56h48v48Zm96-64H152a16,16,0,0,0-16,16v48a16,16,0,0,0,16,16h48a16,16,0,0,0,16-16V56A16,16,0,0,0,200,40Zm0,64H152V56h48v48Zm-96,32H56a16,16,0,0,0-16,16v48a16,16,0,0,0,16,16h48a16,16,0,0,0,
                         16-16V152A16,16,0,0,0,104,136Zm0,64H56V152h48v48Zm96-64H152a16,16,0,0,0-16,16v48a16,16,0,0,0,16,16h48a16,16,0,0,0,16-16V152A16,16,0,0,0,200,136Zm0,64H152V152h48v48Z"></path></svg>
                 </div>
