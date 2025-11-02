f206bb6051593a16294c910c0ef15f12e725ba83
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-11-02 10:40:46 +0600

    update

--- resources/views/livewire/backend/user/orders/sold-orders.blade.php
+++ resources/views/livewire/backend/user/orders/sold-orders.blade.php
@@ -6,24 +6,24 @@
 
                 <div class="relative w-full sm:w-56">
                     <x-ui.select>
-                        <option value="">All statuses</option>
-                        <option value="completed">Completed</option>
-                        <option value="pending">Pending</option>
-                        <option value="processing">Processing</option>
+                        <option value="">{{ __('All statuses') }}</option>
+                        <option value="completed">{{ __('Completed') }}</option>
+                        <option value="pending">{{ __('Pending') }}</option>
+                        <option value="processing">{{ __('Processing') }}</option>
                     </x-ui.select>
                 </div>
 
                 <div class="relative w-full sm:w-56">
                     <x-ui.select>
-                        <option value="">All Game</option>
-                        <option value="game1">Game 1</option>
-                        <option value="game2">Game 2</option>
-                        <option value="game3">Game 3</option>
+                        <option value="">{{ __('All Game') }}</option>
+                        <option value="game1">{{ __('Game 1') }}</option>
+                        <option value="game2">{{ __('Game 2') }}</option>
+                        <option value="game3">{{ __('Game 3') }}</option>
                     </x-ui.select>
                 </div>
 
                 <div class="relative w-full sm:w-56">
-                    <x-ui.input type="text" placeholder="Search" class="pl-5" />
+                    <x-ui.input type="text" placeholder="{{ __('Search') }}" class="pl-5" />
                     <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
                         <x-phosphor-magnifying-glass class="w-5 h-5 fill-text-text-white" />
                     </div>
@@ -34,7 +34,7 @@
             <div class="w-full md:w-auto">
                 <x-ui.button class="w-auto py!">
                     <x-phosphor-download class="w-5 h-5 fill-text-text-white" />
-                    <span>Download invoice</span>
+                    <span>{{ __('Download invoice') }}</span>
                 </x-ui.button>
             </div>
 
@@ -42,7 +42,7 @@
     </div>
     <div>
         <x-ui.user-table :data="$items" :columns="$columns"
-            emptyMessage="No data found. Add your first data to get started." class="rounded-lg overflow-hidden" />
+            emptyMessage="{{ __('No data found. Add your first data to get started.') }}" class="rounded-lg overflow-hidden" />
 
         <x-frontend.pagination-ui :pagination="$pagination" />
     </div>
