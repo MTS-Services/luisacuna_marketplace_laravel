0953de02f7b734b7c6d35b7d3d9578d15eb4d8b8
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-11-04 13:54:09 +0600

    Update shop-component.blade.php

--- resources/views/livewire/frontend/game/shop-component.blade.php
+++ resources/views/livewire/frontend/game/shop-component.blade.php
@@ -18,18 +18,17 @@
                         <span class="text-base font-semibold">Select region</span>
                     </div>
                     <div class="hidden md:flex items-center gap-2">
-                        <div class="">
-                            <span class="text-text-white">
-                                Sort by:
-                            </span>
-                        </div>
-                        <div class="flex items-center gap-2">
-                            <img src="{{ asset('assets/images/gift_cards/Ellipse 4.png') }}" alt="">
-                            <span class="text-text-white">Recommended</span>
-                        </div>
-                        <div class="flex items-center gap-2">
-                            <img src="{{ asset('assets/images/gift_cards/Ellipse 5.png') }}" alt="">
-                            <span class="text-text-white">Lowest Price</span>
+                        <div class="flex gap-3 justify-end">
+                            <button
+                                class="px-4 py-2 border border-green-500 text-green-500 rounded-full text-sm hover:bg-green-500 hover:text-white transition">●
+                                Online Seller</button>
+
+                            {{-- <x-ui.select id="status-select" class="py-0.5! w-auto! pl-5! rounded-full!">
+                                <option value="">Sort by</option>
+                                <option value="">lowest to highest</option>
+                                <option value="">highest to lowest</option>
+                            </x-ui.select> --}}
+
                         </div>
                     </div>
                     <div class="block md:hidden relative z-10" x-data="{ open: false }">
