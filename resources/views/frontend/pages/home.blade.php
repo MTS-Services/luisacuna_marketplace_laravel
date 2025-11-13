fcd54cff2807c50588d348bc1144cf0693ccb80a
Md Shariful Islam <devshariful99@gmail.com>
2025-11-13 14:22:55 +0600

    Update home.blade.php

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -1,7 +1,5 @@
 <x-frontend::app>
     <x-slot name="title">{{ __('Home') }}</x-slot>
     <x-slot name="pageSlug">{{ __('home') }}</x-slot>
-    {{-- <x-tiny-m-c-e-config /> --}}
     <livewire:frontend.home />
-    {{-- <livewire:livewire-example /> --}}
 </x-frontend::app>
