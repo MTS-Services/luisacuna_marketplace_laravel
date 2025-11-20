d868fe9426f407bae218fa5159ae6c50e001cc02
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-11-20 11:37:55 +0600

    Update user-navigation.blade.php

--- resources/views/partials/user-navigation.blade.php
+++ resources/views/partials/user-navigation.blade.php
@@ -1,94 +1,38 @@
-<nav class="hidden xl:flex gap-8 text-sm items-center" x-clock>
-    @foreach (gameCategories() as $category)
-        <a href="{{ $category['url'] }}" wire:navigate
-            x-on:mouseenter="open = (open == '{{ $category['slug'] }}' || open == '' || open != '{{ $category['slug'] }}' ? '{{ $category['slug'] }}' : '')"
-            class="navbar_style group relative"
-            :class="{
-                'active': open == '{{ $category['slug'] }}' ||
-                    {{ request()->routeIs($category['slug']) ? 'true' : 'false' }}
-            }">
-            <span class="relative z-10">{{ $category['name'] }}</span>
-            <span
-                class="absolute bottom-0 left-0 w-full h-0.5 bg-blue-500 transform scale-x-0 transition-transform duration-300 ease-in-out origin-left"
+<nav class="hidden xl:flex gap-8 text-sm items-center relative" x-data="{ open: '', searchActive: false }" x-cloak>
+
+    <!-- Categories -->
+    <div x-show="!searchActive" class="flex gap-8" x-transition:opacity.duration.300ms>
+        @foreach (gameCategories() as $category)
+            <a href="{{ $category['url'] }}" wire:navigate x-on:mouseenter="open = '{{ $category['slug'] }}'"
+                class="navbar_style group relative"
                 :class="{
-                    '!scale-x-100': open == '{{ $category['slug'] }}' ||
+                    'active': open == '{{ $category['slug'] }}' ||
                         {{ request()->routeIs($category['slug']) ? 'true' : 'false' }}
-                }"></span>
-        </a>
-    @endforeach
+                }">
+                <span class="relative z-10">{{ $category['name'] }}</span>
+                <span
+                    class="absolute bottom-0 left-0 w-full h-0.5 bg-blue-500 transform scale-x-0 transition-transform duration-300 ease-in-out origin-left"
+                    :class="{
+                        '!scale-x-100': open == '{{ $category['slug'] }}' ||
+                            {{ request()->routeIs($category['slug']) ? 'true' : 'false' }}
+                    }"></span>
+            </a>
+        @endforeach
+    </div>
 
-    {{-- @foreach (gameCategories() as $category)
-        <a href="{{ $category['url'] }}" wire:navigate
-            x-on:mouseenter="open = (open == '{{ $category['slug'] }}' || open == '' || open != '{{ $category['slug'] }}' ? '{{ $category['slug'] }}' : '')"
-            class="navbar_style group"
-            :class="{ 'active': open == '{{ $category['slug'] }}' ||
-                    {{ request()->route('categorySlug') == $category['slug'] ? 'true' : 'false' }} }">
-            <span class="relative z-10">{{ $category['name'] }}</span>
-            <span class="navbar_indicator" :class="{'active' : open == '{{$category['slug']}}'  || {{ request()->route('categorySlug') == $category['slug'] ? 'true' : 'false' }} }"></span>
-        </a>
-    @endforeach --}}
-    {{-- <button 
-        x-on:mouseenter="open = (open == 'currency' || open == '' || open != 'currency' ? 'currency' : ''); $wire.toggleDropdown('currency')"
-        class="navbar_style group " :class="{'active' : open == 'currency' || {{ request()->has('game-category') && request()->get('game-category') == 'currency' ? 'true' : 'false' }} }">
-        <span class="relative z-10">Currency</span>
-        <span class="navbar_indicator" :class="{'active' : open == 'currency' || {{ request()->has('game-category') && request()->get('game-category') == 'currency' ? 'true' : 'false' }} }"></span>
-    </button>
+    <div class="relative flex items-center justify-end overflow-hidden transition-all duration-300 ease-in-out " :class="searchActive ? 'w-3xl' : 'w-22'"
+        x-transition:enter.duration.300ms x-transition:leave.duration.300ms
+        x-transition:enter-start="w-22" x-transition:enter-end="w-3xl">
 
-
-    <button 
-        x-on:mouseenter="open = (open == 'gift-cards' || open == '' || open != 'gift-cards' ? 'gift-cards' : ''); $wire.toggleDropdown('gift-cards')"
-        class="navbar_style group " :class="{'active' : open == 'gift-cards' || {{request()->has('game-category' ) && request()->get('game-category') == 'gift-cards' ? 'true' : 'false' }} }">
-        <span class="relative z-10">Gift Cards</span>
-        <span class="navbar_indicator " :class="{'active' : open == 'gift-cards' || {{request()->has('game-category' ) && request()->get('game-category') == 'gift-cards' ? 'true' : 'false' }} }"></span>
-    </button>
-
-
-    <button 
-        x-on:mouseenter="open = (open == 'boosting' || open == '' || open != 'boosting' ? 'boosting' : ''); $wire.toggleDropdown('boosting')"
-        class="navbar_style group" :class="{'active' : open == 'boosting' || {{request()->has('game-category' ) && request()->get('game-category') == 'boosting' ? 'true' : 'false' }} }">
-        <span class="relative z-10">Boosting</span>
-        <span class="navbar_indicator" :class="{'active' : open == 'boosting' || {{request()->has('game-category' ) && request()->get('game-category') == 'boosting' ? 'true' : 'false' }} }"></span>
-    </button>
-
-
-    <button 
-        x-on:mouseenter="open = (open == 'items' || open == '' || open != 'items' ? 'items' : ''); $wire.toggleDropdown('items')"
-        class="navbar_style group" :class="{'active' : open == 'items' || {{request()->has('game-category' ) && request()->get('game-category') == 'items' ? 'true' : 'false' }} }">
-        <span class="relative z-10">Items</span>
-        <span class="navbar_indicator" :class="{'active' : open == 'items' || {{request()->has('game-category' ) && request()->get('game-category') == 'items' ? 'true' : 'false' }} }"></span>
-    </button>
-
-
-    <button 
-        x-on:mouseenter="open = (open == 'accounts' || open == '' || open != 'accounts' ? 'accounts' : ''); $wire.toggleDropdown('accounts')"
-        class="navbar_style group" :class="{'active' : open == 'accounts' || {{request()->has('game-category' ) && request()->get('game-category') == 'accounts' ? 'true' : 'false' }} }">
-        <span class="relative z-10">Accounts</span>
-        <span class="navbar_indicator" :class="{'active' : open == 'accounts' || {{request()->has('game-category' ) && request()->get('game-category') == 'accounts' ? 'true' : 'false' }} }"></span>
-    </button>
-
-
-    <button 
-        x-on:mouseenter="open = (open == 'topups' || open == '' || open != 'topups' ? 'topups' : ''); $wire.toggleDropdown('topups')"
-        class="navbar_style group" :class="{'active' : open == 'topups' || {{request()->has('game-category' ) && request()->get('game-category') == 'topups' ? 'true' : 'false' }} }">
-        <span class="relative z-10">Top Ups</span>
-        <span class="navbar_indicator" :class="{'active' : open == 'topups' || {{request()->has('game-category' ) && request()->get('game-category') == 'topups' ? 'true' : 'false' }} }"></span>
-    </button>
-
-
-    <button 
-        x-on:mouseenter="open = (open == 'coaching' || open == '' || open != 'coaching' ? 'coaching' : ''); $wire.toggleDropdown('coaching')"
-        class="navbar_style group" :class="{'active' : open == 'coaching' || {{request()->has('game-category' ) && request()->get('game-category') == 'coaching' ? 'true' : 'false' }} }">
-        <span class="relative z-10">Coaching</span>
-        <span class="navbar_indicator" :class="{'active' : open == 'coaching' || {{request()->has('game-category' ) && request()->get('game-category') == 'coaching' ? 'true' : 'false' }} }"></span>
-    </button> --}}
-    <div class="relative hidden xl:block">
         <flux:icon name="magnifying-glass"
             class="w-4 h-4 absolute left-3 top-1/2 transform -translate-y-1/2 stroke-text-primary" />
 
-        <input type="text" placeholder="Search" x-on:click="globalSearchModal = true" x-clock
-            class="border dark:border-white border-gray-600 rounded-full py-1.5 pl-8 pr-2 text-sm focus:outline-none focus:border-purple-500 focus:bg-bg-primary transition-all w-22 bg-transparent placeholder:text-text-primary">
+        <input type="text" placeholder="Search" x-on:click="searchActive = true; open = ''"
+            x-on:blur="searchActive = false"
+            class="border dark:border-white border-gray-600 rounded-full py-2 pl-8 pr-2 text-sm focus:outline-none focus:border-purple-500 focus:bg-bg-primary w-full bg-transparent placeholder:text-text-primary">
     </div>
 
 
-      <livewire:frontend.partials.search-modal />
+    <livewire:frontend.partials.search-modal />
+
 </nav>
