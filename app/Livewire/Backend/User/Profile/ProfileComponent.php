6b33d58956b39cdf3a0c777c7c46b3c7050cf197
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-11-24 16:16:34 +0600

    Update ProfileComponent.php

--- app/Livewire/Backend/User/Profile/ProfileComponent.php
+++ app/Livewire/Backend/User/Profile/ProfileComponent.php
@@ -7,23 +7,23 @@ use Livewire\Component;
 class ProfileComponent extends Component
 {
 
-    // public $activeTab = 'currency';
-    public $reviewItem = 'all';
+    // // public $activeTab = 'currency';
+    // public $reviewItem = 'all';
 
-    public $activeInnerMenu = 'shop';
-    public function switchTab($tab)
-    {
-        $this->activeTab = $tab;
-    }
+    // public $activeInnerMenu = 'shop';
+    // public function switchTab($tab)
+    // {
+    //     $this->activeTab = $tab;
+    // }
 
-    public function switchInnerMenu($menu)
-    {
-        $this->activeInnerMenu = $menu;
-    }
-    public function switchReviewItem($item)
-    {
-        $this->reviewItem = $item;
-    }
+    // public function switchInnerMenu($menu)
+    // {
+    //     $this->activeInnerMenu = $menu;
+    // }
+    // public function switchReviewItem($item)
+    // {
+    //     $this->reviewItem = $item;
+    // }
 
     public function render()
     {
