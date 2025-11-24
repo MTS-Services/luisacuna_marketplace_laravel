7c1c95fc7b96148370bd8de665da87547a123960
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-11-24 12:48:13 +0600

    create components

---
+++ app/Livewire/Backend/User/Profile/Shop.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Backend\User\Profile;
+
+use Livewire\Component;
+
+class Shop extends Component
+{
+    public function render()
+    {
+        return view('livewire.backend.user.profile.shop');
+    }
+}
