838816ddf7f42f67629e52e970328da52fa74743
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-11-25 14:35:43 +0600

    Update Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -68,12 +68,7 @@ class Category extends AuditBaseModel implements Auditable
 
     public function games(): BelongsToMany
     {
-        return $this->belongsToMany(
-            Game::class,
-            'game_categories',
-            'category_id',
-            'game_id'
-        );
+        return $this->belongsToMany(Game::class,'game_categories','category_id','game_id' );
     }
 
     public function achievements(): HasMany
