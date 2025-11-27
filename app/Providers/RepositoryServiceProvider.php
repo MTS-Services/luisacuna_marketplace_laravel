96c07dc00e71d077887a909e8ab2e125a4b3a63a
Md Shariful Islam <devshariful99@gmail.com>
2025-11-27 17:15:34 +0600

    Game clean --done

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -3,9 +3,7 @@
 namespace App\Providers;
 
 use App\Repositories\Contracts\TagRepositoryInterface;
-use App\Repositories\Contracts\TypeRepositoryInterface;
 use App\Repositories\Eloquent\TagRepository;
-use App\Repositories\Eloquent\TypeRepository;
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\Eloquent\GameRepository;
 use App\Repositories\Eloquent\RankRepository;
@@ -13,18 +11,15 @@ use App\Repositories\Eloquent\RoleRepository;
 use App\Repositories\Eloquent\UserRepository;
 use App\Repositories\Eloquent\AdminRepository;
 use App\Repositories\Eloquent\AuditRepository;
-use App\Repositories\Eloquent\ServerRepository;
 use App\Repositories\Eloquent\ProductRepository;
 use App\Repositories\Eloquent\CategoryRepository;
 use App\Repositories\Eloquent\CurrencyRepository;
 use App\Repositories\Eloquent\LanguageRepository;
 use App\Repositories\Eloquent\PageViewRepository;
 use App\Repositories\Eloquent\OfferItemRepository;
-use App\Repositories\Eloquent\GameServerRepository;
 
 
 use App\Repositories\Eloquent\PermissionRepository;
-use App\Repositories\Eloquent\PlatformRepository;
 use App\Repositories\Eloquent\ProductTypeRepository;
 use App\Repositories\Eloquent\AchievementRepository;
 
@@ -46,20 +41,9 @@ use App\Repositories\Contracts\PermissionRepositoryInterface;
 use App\Repositories\Contracts\AchievementRepositoryInterface;
 use App\Repositories\Contracts\ProductTypeRepositoryInterface;
 use App\Repositories\Contracts\AchievementTypeRepositoryInterface;
-use App\Repositories\Contracts\GamePlatformRepositoryInterface;
-use App\Repositories\Contracts\GameRarityRepositoryInterface;
-use App\Repositories\Contracts\GameServerRepositoryInterface;
 use App\Repositories\Contracts\GameTagRepositoryInterface;
-use App\Repositories\Contracts\GameTypeRepositoryInterface;
-use App\Repositories\Contracts\PlatformRepositoryInterface;
-use App\Repositories\Contracts\ServerRepositoryInterface;
 use App\Repositories\Contracts\OfferItemRepositoryInterface;
-use App\Repositories\Contracts\RarityRepositoryInterface;
-use App\Repositories\Eloquent\GamePlatformRepository;
-use App\Repositories\Eloquent\GameRarityRepository;
 use App\Repositories\Eloquent\GameTagRepository;
-use App\Repositories\Eloquent\GameTypeRepository;
-use App\Repositories\Eloquent\RaritytRepository;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
@@ -88,51 +72,11 @@ class RepositoryServiceProvider extends ServiceProvider
             GameRepository::class,
         );
 
-        //Platform Repository
-        $this->app->bind(
-            PlatformRepositoryInterface::class,
-            PlatformRepository::class
-        );
-
-
-        // Server Repository
-        $this->app->bind(
-
-            ServerRepositoryInterface::class,
-            ServerRepository::class,
-
-        );
-
-        // Game Server Repository
-        $this->app->bind(
-
-            GameServerRepositoryInterface::class,
-            GameServerRepository::class,
-        );
-
 
         // Game Tag Repository End
           $this->app->bind(
             GameTagRepositoryInterface::class,
             GameTagRepository::class,
-        );
-
-        // Game Type Repository End
-          $this->app->bind(
-            GameTypeRepositoryInterface::class,
-            GameTypeRepository::class,
-        );
-
-        // Game Platform Repository End
-          $this->app->bind(
-            GamePlatformRepositoryInterface::class,
-            GamePlatformRepository::class,
-        );
-
-        // Game Platform Repository End
-          $this->app->bind(
-            GameRarityRepositoryInterface::class,
-            GameRarityRepository::class,
         );
 
         // Rank
@@ -153,14 +97,6 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(
             CurrencyRepositoryInterface::class,
             CurrencyRepository::class
-        );
-        $this->app->bind(
-            ProductTypeRepositoryInterface::class,
-            ProductTypeRepository::class
-        );
-        $this->app->bind(
-            ProductRepositoryInterface::class,
-            ProductRepository::class
         );
         $this->app->bind(
             PageViewRepositoryInterface::class,
@@ -185,14 +121,6 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(
             OfferItemRepositoryInterface::class,
             OfferItemRepository::class
-        );
-        $this->app->bind(
-            RarityRepositoryInterface::class,
-            RaritytRepository::class
-        );
-        $this->app->bind(
-            TypeRepositoryInterface::class,
-            TypeRepository::class
         );
         $this->app->bind(
             TagRepositoryInterface::class,
--- app/Repositories/Contracts/GameTypeRepositoryInterface.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-namespace App\Repositories\Contracts;
-
-interface GameTypeRepositoryInterface
-{
-    public function getQuery(): object;
-}   
