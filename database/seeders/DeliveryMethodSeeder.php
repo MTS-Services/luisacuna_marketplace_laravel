be1f14404c62a87aae6e47f4ca175265d822ba67
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-12-13 15:10:25 +0600

    fixed seeder

--- database/seeders/DeliveryMethodSeeder.php
+++ database/seeders/DeliveryMethodSeeder.php
@@ -12,6 +12,17 @@ class DeliveryMethodSeeder extends Seeder
      */
     public function run(): void
     {
-        DeliveryMethod::factory(10)->create();
+        DeliveryMethod::insert([
+            [
+                'name' => 'Manual Delivery',
+                'slug' => 'manual-delivery',
+                'image' => 'https://placehold.co/400x400/ff0000/ffffff?text=Manual+Delivery',
+            ],
+            [
+                'name' => 'instant Delivery',
+                'slug' => 'instant-delivery',
+                'image' => 'https://placehold.co/400x400/00ff00/ffffff?text=Instant+Delivery',
+            ],
+        ]);
     }
 }
