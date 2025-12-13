d6c1cbaf2cd63fd7653af0b3c48183cb26eefb05
Md Monirul Islam Taluqdar <82707883+monirulislam0@users.noreply.github.com>
2025-12-13 17:59:45 +0600

    Updated

---
+++ database/migrations/2025_12_13_112450_create_seller_kycs_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Enums\SellerKycStatus;
+use App\Traits\AuditColumnsTrait;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+     use AuditColumnsTrait;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('seller_kycs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('sort_order')->default(0)->index();
+            $table->unsignedBigInteger('seller_id')->index();
+            $table->unsignedBigInteger('service_category_id')->index();
+            
+            $table->string('first_name')->index();
+            $table->string('last_name')->index();
+            $table->date('dob');
+            $table->string('nationality');
+            $table->string('address');
+            $table->string('city');
+            $table->string('country');
+            $table->string('postal_code');
+            $table->string('document_type');
+            $table->string('front_image');
+            $table->string('back_image')->nuallble();
+            $table->string('seller_experience');
+            $table->string('status')->index()->default(SellerKycStatus::PENDING->value);
+
+
+            $table->softDeletes();
+            $table->timestamps();
+
+            $this->addAdminAuditColumns($table);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('seller_kycs');
+    }
+};
