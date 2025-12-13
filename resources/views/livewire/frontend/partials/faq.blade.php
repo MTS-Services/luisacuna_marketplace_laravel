a9aeaf3e6448bc232e6f3152296ff17e0da1de72
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-12-13 14:37:43 +0600

    Revert "Merge pull request #206 from MTS-Services/frontend_dev"
    This reverts commit d47849085e2a457211331c4c77f524b60f917497, reversing
    changes made to 781c38eb13c682414ad87a94ebd190b7e45c491e.

--- resources/views/livewire/frontend/partials/faq.blade.php
+++ resources/views/livewire/frontend/partials/faq.blade.php
@@ -1,25 +1,21 @@
-    <section class="py-20 ">
-        <div class="bg-bg-secondary container p-18 flex justify-center wrap flex-col rounded-3xl" x-data="{ active: 0, tab: 'buyers' }">
+    <section class="py-20 bg-bg-primary ">
+        <div class="container py-8 flex justify-center wrap flex-col" x-data="{ active: 0, tab: 'buyers' }">
 
-            <h2 class="text-text-white text-center text-5xl  mb-6 font-semibold">{{ 'Frequently Asked Questions' }}
+            <h2 class="text-text-white text-center  mb-6 font-semibold">{{ 'Frequently Asked Questions' }}
             </h2>
 
-            <h5 class="text-text-white text-center text-2xl mb-6 font-medium">
-                {{ 'Everything you need to know about buying and selling on Swapy.gg.' }}</h5>
-
             <!-- Tabs -->
-            <div class="max-w-xs mx-auto flex justify-between mb-8  rounded-full px-4 py-4">
-                <button @click="tab = 'buyers'; active = 0"
-                    :class="tab === 'buyers' ? 'bg-zinc-800 px-8 py-5 rounded-full shadow-lg text-gray-200' :
-                        'text-text-secondery px-6 py-4'"
-                    class="transition-colors duration-300 text-lg">
+            <div class="max-w-xs mx-auto flex justify-between mb-8 bg-bg-info rounded-full px-3 py-3">
+                <button @click="tab = 'buyers'; active = 0" 
+                    :class="tab === 'buyers' ? 'bg-bg-secondary px-5 py-3 rounded-full shadow-lg text-white' :
+                        'text-text-secondery px-5 py-3'"
+                    class="transition-colors duration-300 font-medium">
                     {{ 'For Buyers' }}
                 </button>
-                <button @click="tab = 'sellers'; active = 0"
-                    :class="tab === 'sellers' ?
-                        'bg-bg-secondary px-8 py-5 rounded-full shadow-lg dark:bg-zinc-800 text-gray-400' :
-                        'text-text-secondery px-6 py-4'"
-                    class="transition-colors duration-300 text-lg">
+                <button @click="tab = 'sellers'; active = 0" 
+                    :class="tab === 'sellers' ? 'bg-bg-secondary px-5 py-3 rounded-full shadow-lg text-white' :
+                        'text-text-secondery px-5 py-3'"
+                    class="transition-colors duration-300 font-medium">
                     {{ __('For Sellers') }}
                 </button>
             </div>
@@ -28,25 +24,27 @@
             <template x-if="tab === 'buyers'">
                 <div class="space-y-4">
 
-                    @foreach ($faqs_buyer as $index => $faq)
-                        <div class="dark:bg-bg-border2 bg-bg-secondary rounded-xl p-10 cursor-pointer"
-                            @click="active === {{ $index }} ? active = null : active = {{ $index }}">
-                            <div class="flex justify-between items-center">
-                                <h3 class="text-text-white font-semibold">{{ $faq->question }}</h3>
-                                <svg :class="active === 0 ? 'rotate-180' : ''"
-                                    class="w-5 h-5 text-text-white transition-transform" fill="none"
-                                    stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
-                                </svg>
-                            </div>
-                            <p x-show="active === {{ $index }}" x-transition
-                                class="mt-2 text-text-secondery text-sm">
-                                {{ $faq->answer }}
-                            </p>
+                    @foreach ($faqs_buyer as $index => $faq )
+
+                  
+
+                        <div class="bg-bg-secondary rounded-xl p-4 cursor-pointer"
+                        @click="active === {{ $index }} ? active = null : active = {{$index}}">
+                        <div class="flex justify-between items-center">
+                            <h3 class="text-text-white font-semibold">{{ $faq->question }}</h3>
+                            <svg :class="active === 0 ? 'rotate-180' : ''"
+                                class="w-5 h-5 text-text-white transition-transform" fill="none"
+                                stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+                            </svg>
                         </div>
+                        <p x-show="active === {{ $index }}" x-transition class="mt-2 text-text-secondery text-sm">
+                            {{ $faq->answer }}
+                        </p>
+                    </div>
                     @endforeach
                     <!-- Buyer FAQ Items -->
-
+                    
 
                 </div>
             </template>
@@ -55,22 +53,21 @@
             <template x-if="tab === 'sellers'">
                 <div class="space-y-4">
 
-                    @foreach ($faqs_seller as $index => $faq)
+                 @foreach ($faqs_seller as $index => $faq )
                         <div class="bg-bg-secondary rounded-xl p-4 cursor-pointer"
-                            @click="active === {{ $index }} ? active = null : active = {{ $index }}">
-                            <div class="flex justify-between items-center">
-                                <h3 class="text-text-white font-semibold">{{ $faq->question }}</h3>
-                                <svg :class="active === 0 ? 'rotate-180' : ''"
-                                    class="w-5 h-5 text-text-white transition-transform" fill="none"
-                                    stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
-                                </svg>
-                            </div>
-                            <p x-show="active === {{ $index }}" x-transition
-                                class="mt-2 text-text-secondery text-sm">
-                                {{ $faq->answer }}
-                            </p>
+                        @click="active === {{ $index }} ? active = null : active = {{$index}}">
+                        <div class="flex justify-between items-center">
+                            <h3 class="text-text-white font-semibold">{{ $faq->question }}</h3>
+                            <svg :class="active === 0 ? 'rotate-180' : ''"
+                                class="w-5 h-5 text-text-white transition-transform" fill="none"
+                                stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+                            </svg>
                         </div>
+                        <p x-show="active === {{ $index }}" x-transition class="mt-2 text-text-secondery text-sm">
+                            {{ $faq->answer }}
+                        </p>
+                    </div>
                     @endforeach
 
 
