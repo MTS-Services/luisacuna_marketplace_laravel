3e37187102ba6ecf40057e8a2f8084e3a55617df
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-12-22 11:40:14 +0600

    component add done

---
+++ app/Livewire/Backend/Admin/WithdrawalManagement/WithdrawalMethod/Edit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Backend\Admin\WithdrawalManagement\WithdrawalMethod;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.backend.admin.withdrawal-management.withdrawal-method.edit');
+    }
+}
