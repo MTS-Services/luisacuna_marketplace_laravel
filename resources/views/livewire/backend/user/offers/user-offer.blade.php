93950e9e90a808627ef9deee9ba427545ce3d4ed
Md Monirul Islam Taluqdar <82707883+monirulislam0@users.noreply.github.com>
2025-12-23 19:12:53 +0600

    updated

--- resources/views/livewire/backend/user/offers/user-offer.blade.php
+++ resources/views/livewire/backend/user/offers/user-offer.blade.php
@@ -1,48 +1,28 @@
 <div class="space-y-6">
     <div class="p-4 w-full">
-        <div class="flex flex-col md:flex-row justify-between items-stretch md:items-center gap-3 md:gap-4">
+        <div class="flex flex-col lg:flex-row justify-between items-stretch md:items-center gap-3 md:gap-4">
 
-            <div class="flex flex-col sm:flex-row gap-3 w-full md:w-auto">
+            <div class="flex flex-col sm:flex-row justify-between items-center gap-3 w-full md:w-auto">
 
                 <!-- Game Filter -->
-                <div class="relative w-full sm:w-40 md:w-44">
-                    <x-ui.select class="bg-surface-card border border-border-primary py-1.5! rounded-lg">
-                        <option value="">{{ __('All Game') }}</option>
-                        {{-- @foreach ($games as $game)
-                            <option value="{{ $game->id }}">{{ $game->name }}</option>
-                        @endforeach --}}
-                    </x-ui.select>
-                </div>
-
-                <!-- Status Filter -->
-                <div class="relative w-full sm:w-40 md:w-44">
-                    <x-ui.select class="bg-surface-card border border-border-primary py-1.5! rounded-lg">
-                        <option value="">{{ __('All') }}</option>
-                        <option value="active">{{ __('Active offers') }}</option>
-                        <option value="paused">{{ __('Paused offers') }}</option>
-                        <option value="closed">{{ __('Closed offers') }}</option>
-                    </x-ui.select>
-                </div>
-
-                <!-- Recommended Filter -->
-                <div class="relative w-full sm:w-44 md:w-48">
-                    <x-ui.select class="bg-surface-card border border-border-primary py-1.5! rounded-lg">
-                        <option value="">{{ __('Recommended') }}</option>
-                        <option value="price_low">{{ __('Price: Low to High') }}</option>
-                        <option value="price_high">{{ __('Price: High to Low') }}</option>
-                        <option value="newest">{{ __('Newest First') }}</option>
-                    </x-ui.select>
-                </div>
-
-                <!-- Search Input -->
-                {{-- <div class="relative w-full sm:w-56">
-                    <x-ui.input type="text" placeholder="{{ __('Search') }}" class="pl-5 py-1.5! text-text-white" />
-                    <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-                        <x-phosphor-magnifying-glass class="w-5 h-5 fill-text-text-white" />
-                    </div>
-                </div> --}}
                 <div class="relative w-full sm:w-56">
-                    <x-ui.input type="text" placeholder="{{ __('Search') }}" class="pl-5 py-1.5! text-text-white"
+                        <x-ui.custom-select class="rounded!" label="All Game">
+                            <x-ui.custom-option label="January 2025" />
+                            <x-ui.custom-option label="February 2025" />
+                            <x-ui.custom-option label="March 2025" />
+                        </x-ui.custom-select>
+               </div>
+                  <!-- Game Filter -->
+               <div class="relative w-full sm:w-56">
+                        <x-ui.custom-select class="rounded!" label="All ">
+                            <x-ui.custom-option label="January 2025" />
+                            <x-ui.custom-option label="February 2025" />
+                            <x-ui.custom-option label="March 2025" />
+                        </x-ui.custom-select>
+               </div>
+
+                <div class="relative w-full sm:w-56">
+                    <x-ui.input type="text" placeholder="{{ __('Search') }}" class="pl-5 py-1.5! text-text-white rounded! placeholder:text-text-primary border-zinc-500"
                         wire:model.live.debounce.300ms="search" />
                     <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
                         <x-phosphor-magnifying-glass class="w-5 h-5 fill-text-text-white" />
@@ -50,7 +30,7 @@
                 </div>
             </div>
 
-            <div class="w-full md:w-auto flex items-center gap-2 justify-between">
+            <div class="w-full lg:w-auto flex items-center gap-2 justify-between">
                 <x-ui.button class="w-auto! py-2!" variant="secondary" x-data
                     @click="$dispatch('open-modal', 'export')">
                     <x-phosphor-download class="w-5 h-5 fill-accent group-hover:fill-white" />
