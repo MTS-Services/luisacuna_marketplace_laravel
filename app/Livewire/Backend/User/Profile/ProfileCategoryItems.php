f57271cd9a07966b6e4d18d072e3908249e20c7a
Md Monirul Islam Taluqdar <82707883+monirulislam0@users.noreply.github.com>
2025-12-26 12:00:36 +0600

    updated

--- app/Livewire/Backend/User/Profile/ProfileCategoryItems.php
+++ app/Livewire/Backend/User/Profile/ProfileCategoryItems.php
@@ -2,24 +2,56 @@
 
 namespace App\Livewire\Backend\User\Profile;
 
+use App\Models\Category;
+use App\Models\User;
+use App\Services\CategoryService;
 use App\Services\GameService;
+use App\Services\ProductService;
+use App\Traits\WithPaginationData;
 use Livewire\Component;
 use Livewire\Attributes\Url;
 
 class ProfileCategoryItems extends Component
 {
+    use WithPaginationData;
+
      #[Url(keep: true)]
     public $activeTab = 'currency';
 
+    public $list_type = 'list_grid';
+
      protected GameService $gameService;
-    public function boot(GameService $gameService)
+     protected ProductService $productService;
+     protected CategoryService $categoryService ;
+     protected User $user;
+    public function boot(GameService $gameService, ProductService $productService, CategoryService $categoryService)
     {
+
         $this->gameService = $gameService;
+        $this->productService = $productService;
+        $this->categoryService = $categoryService;
+    }
+
+    public function mount(User $user){
+        $this->user = $user; 
+       $this->list_type = $this->categoryService->findData($this->activeTab, 'slug')->layout->value;
     }
     public function render()
-    {   $games = $this->gameService->getAllDatas();
+    {   $products = $this->getProducts();
+        $games = $this->gameService->getAllDatas();
+        $this->paginationData($products);
         return view('livewire.backend.user.profile.profile-category-items', [
+            'products' => $products,
             'games' => $games,
+            
+        ]);
+    }
+
+    protected function getProducts(){
+       return $this->productService->getPaginatedData($this->perPage, [
+            'categorySlug' => $this->activeTab,
+            'relations' => ['games'], 
+            'user_id' => $this->user->id,
         ]);
     }
 }
--- resources/views/livewire/backend/user/profile/profile-category-items.blade.php
+++ resources/views/livewire/backend/user/profile/profile-category-items.blade.php
@@ -261,7 +261,8 @@
                 <button class="text-text-primary text-sm hover:text-zinc-500">{{ __('Next') }}</button>
             </div>
         </section>
-    @else
+    @endif 
+
         {{-- select game --}}
         <div class="w-full sm:w-sm md:w-md lg:w-md mt-6">
             <x-ui.custom-select wire:model="game_id" id="game_id"
@@ -274,345 +275,28 @@
         </div>
 
         {{-- games --}}
+
+        @if($list_type == 'list_grid')
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
-            <div class="bg-bg-secondary rounded-lg p-5">
-                <div class="flex items-center justify-between gap-4">
-                    <div class="flex items-center gap-2">
-                        <img src="{{ asset('assets/images/user_profile/frame.png') }}" alt="">
-                        <span class="95 text-base-600 font-semibold">{{ __('PlayStation') }}</span>
-                    </div>
-                    <div class="flex items-center">
-                        <div class="flex items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                class="w-10 h-10 text-text-secondary">
-                                <path d="M12 10.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3Z" />
-                            </svg>
-                            <span class="text-text-secondary text-base-600">{{ __('Stacked') }}</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="flex items-center gap-2 mt-5">
-                    <span class="text-base-400">
-                        {{ __('Galaxy Skin – PSN / Xbox / PC Full Access') }}
-                    </span>
-                    <img src="{{ asset('assets/images/user_profile/product-image-container -_ image.png') }}"
-                        alt="">
-                </div>
-                <div class="flex items-center justify-between mt-10">
-                    <div class="">
-                        <span class="text-base-600 font-semibold">
-                            {{ __('PEN175.27') }}
-                        </span>
-                    </div>
-                    <div class="flex
-                                        items-center gap-2">
-                        <flux:icon name="clock" class="w-6 h-6 50" />
-                        <span class="text-xs-400 50">
-                            {{ __('Instant') }}
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <div class="bg-bg-secondary rounded-lg p-5">
-                <div class="flex items-center justify-between gap-4">
-                    <div class="flex items-center gap-2">
-                        <img src="{{ asset('assets/images/user_profile/frame.png') }}" alt="">
-                        <span class="text-[#4ADE80] text-base-400">{{ __('Xbox') }}</span>
-                    </div>
-                    <div class="flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                            class="w-10 h-10 text-text-secondary">
-                            <path d="M12 10.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3Z" />
-                        </svg>
-                        <span class="text-text-secondary text-base-600">{{ __('Stacked') }}</span>
-                    </div>
-                </div>
-                <div class="flex items-center mt-5">
-                    <span class="text-base-400">
-                        {{ __('Blue Squire Skin – 50 VB – Xbox / PSN / PC Full Access') }}
-                    </span>
-                    <img src="{{ asset('assets/images/user_profile/e84f9097828ae420d3f6578c742ab821a27d643b.png') }}"
-                        alt="">
-                </div>
-                <div class="flex items-center justify-between mt-10">
-                    <div class="">
-                        <span class="text-base-600 font-semibold">
-                            {{ __('PEN175.27') }}
-                        </span>
-                    </div>
-                    <div class="flex
-                                        items-center gap-2">
-                        <flux:icon name="clock" class="w-6 h-6 50" />
-                        <span class="text-xs-400 50">
-                            {{ __('Instant') }}
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <div class="bg-bg-secondary rounded-lg p-5">
-                <div class="flex items-center justify-between gap-4">
-                    <div class="flex items-center gap-2">
-                        <img src="{{ asset('assets/images/user_profile/frame.png') }}" alt="">
-                        <span class="text-[#4ADE80] text-base-400">{{ __('Xbox') }}</span>
-                    </div>
-                    <div class="flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                            class="w-10 h-10 text-text-secondary">
-                            <path d="M12 10.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3Z" />
-                        </svg>
-                        <span class="text-text-secondary text-base-600">{{ __('Stacked') }}</span>
-                    </div>
-                </div>
-                <div class="flex items-center mt-5">
-                    <span class="text-base-400">
-                        {{ __('Blue Squire Skin – 50 VB – Xbox / PSN / PC Full Access') }}
-                    </span>
-                    <img src="{{ asset('assets/images/user_profile/e84f9097828ae420d3f6578c742ab821a27d643b.png') }}"
-                        alt="">
-                </div>
-                <div class="flex items-center justify-between mt-10">
-                    <div class="">
-                        <span class="text-base-600 font-semibold">
-                            {{ __('PEN175.27') }}
-                        </span>
-                    </div>
-                    <div class="flex
-                                        items-center gap-2">
-                        <flux:icon name="clock" class="w-6 h-6 50" />
-                        <span class="text-xs-400 50">
-                            {{ __('Instant') }}
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <div class="bg-bg-secondary rounded-lg p-5">
-                <div class="flex items-center justify-between gap-4">
-                    <div class="flex items-center gap-2">
-                        <img src="{{ asset('assets/images/user_profile/frame.png') }}" alt="">
-                        <span class="text-[#4ADE80] text-base-400">{{ __('Xbox') }}</span>
-                    </div>
-                    <div class="flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                            class="w-10 h-10 text-text-secondary">
-                            <path d="M12 10.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3Z" />
-                        </svg>
-                        <span class="text-text-secondary text-base-600">{{ __('Stacked') }}</span>
-                    </div>
-                </div>
-                <div class="flex items-center mt-5">
-                    <span class="text-base-400">
-                        {{ __('Blue Squire Skin – 50 VB – Xbox / PSN / PC Full Access') }}
-                    </span>
-                    <img src="{{ asset('assets/images/user_profile/e84f9097828ae420d3f6578c742ab821a27d643b.png') }}"
-                        alt="">
-                </div>
-                <div class="flex items-center justify-between mt-10">
-                    <div class="">
-                        <span class="text-base-600 font-semibold">
-                            {{ __('PEN175.27') }}
-                        </span>
-                    </div>
-                    <div class="flex
-                                        items-center gap-2">
-                        <flux:icon name="clock" class="w-6 h-6 50" />
-                        <span class="text-xs-400 50">
-                            {{ __('Instant') }}
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <div class="bg-bg-secondary rounded-lg p-5">
-                <div class="flex items-center justify-between gap-4">
-                    <div class="flex items-center gap-2">
-                        <img src="{{ asset('assets/images/user_profile/frame.png') }}" alt="">
-                        <span class="text-[#4ADE80] text-base-400">{{ __('Xbox') }}</span>
-                    </div>
-                    <div class="flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                            class="w-10 h-10 text-text-secondary">
-                            <path d="M12 10.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3Z" />
-                        </svg>
-                        <span class="text-text-secondary text-base-600">{{ __('Stacked') }}</span>
-                    </div>
-                </div>
-                <div class="flex items-center mt-5">
-                    <span class="text-base-400">
-                        {{ __('Blue Squire Skin – 50 VB – Xbox / PSN / PC Full Access') }}
-                    </span>
-                    <img src="{{ asset('assets/images/user_profile/e84f9097828ae420d3f6578c742ab821a27d643b.png') }}"
-                        alt="">
-                </div>
-                <div class="flex items-center justify-between mt-10">
-                    <div class="">
-                        <span class="text-base-600 font-semibold">
-                            {{ __('PEN175.27') }}
-                        </span>
-                    </div>
-                    <div class="flex
-                                        items-center gap-2">
-                        <flux:icon name="clock" class="w-6 h-6 50" />
-                        <span class="text-xs-400 50">
-                            {{ __('Instant') }}
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <div class="bg-bg-secondary rounded-lg p-5">
-                <div class="flex items-center justify-between gap-4">
-                    <div class="flex items-center gap-2">
-                        <img src="{{ asset('assets/images/user_profile/frame.png') }}" alt="">
-                        <span class="text-[#4ADE80] text-base-400">{{ __('Xbox') }}</span>
-                    </div>
-                    <div class="flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                            class="w-10 h-10 text-text-secondary">
-                            <path d="M12 10.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3Z" />
-                        </svg>
-                        <span class="text-text-secondary text-base-600">{{ __('Stacked') }}</span>
-                    </div>
-                </div>
-                <div class="flex items-center mt-5">
-                    <span class="text-base-400">
-                        {{ __('Blue Squire Skin – 50 VB – Xbox / PSN / PC Full Access') }}
-                    </span>
-                    <img src="{{ asset('assets/images/user_profile/e84f9097828ae420d3f6578c742ab821a27d643b.png') }}"
-                        alt="">
-                </div>
-                <div class="flex items-center justify-between mt-10">
-                    <div class="">
-                        <span class="text-base-600 font-semibold">
-                            {{ __(' PEN175.27') }}
-                        </span>
-                    </div>
-                    <div class="flex
-                                        items-center gap-2">
-                        <flux:icon name="clock" class="w-6 h-6 50" />
-                        <span class="text-xs-400 50">
-                            {{ __('Instant') }}
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <div class="bg-bg-secondary rounded-lg p-5">
-                <div class="flex items-center justify-between gap-4">
-                    <div class="flex items-center gap-2">
-                        <img src="{{ asset('assets/images/user_profile/frame.png') }}" alt="">
-                        <span class="text-[#4ADE80] text-base-400">{{ __('Xbox') }}</span>
-                    </div>
-                    <div class="flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                            class="w-10 h-10 text-text-secondary">
-                            <path d="M12 10.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3Z" />
-                        </svg>
-                        <span class="text-text-secondary text-base-600">{{ __('Stacked') }}</span>
-                    </div>
-                </div>
-                <div class="flex items-center mt-5">
-                    <span class="text-base-400">
-                        {{ __('Blue Squire Skin – 50 VB – Xbox / PSN / PC Full Access') }}
-                    </span>
-                    <img src="{{ asset('assets/images/user_profile/e84f9097828ae420d3f6578c742ab821a27d643b.png') }}"
-                        alt="">
-                </div>
-                <div class="flex items-center justify-between mt-10">
-                    <div class="">
-                        <span class="text-base-600 font-semibold">
-                            {{ __('PEN175.27') }}
-                        </span>
-                    </div>
-                    <div class="flex
-                                        items-center gap-2">
-                        <flux:icon name="clock" class="w-6 h-6 50" />
-                        <span class="text-xs-400 50">
-                            {{ __('Instant') }}
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <div class="bg-bg-secondary rounded-lg p-5">
-                <div class="flex items-center justify-between gap-4">
-                    <div class="flex items-center gap-2">
-                        <img src="{{ asset('assets/images/user_profile/frame.png') }}" alt="">
-                        <span class="text-[#4ADE80] text-base-400">{{ __('Xbox') }}</span>
-                    </div>
-                    <div class="flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                            class="w-10 h-10 text-text-secondary">
-                            <path d="M12 10.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3Z" />
-                        </svg>
-                        <span class="text-text-secondary text-base-600">{{ __('Stacked') }}</span>
-                    </div>
-                </div>
-                <div class="flex items-center mt-5">
-                    <span class="text-base-400">
-                        {{ __('Blue Squire Skin – 50 VB – Xbox / PSN / PC Full Access') }}
-                    </span>
-                    <img src="{{ asset('assets/images/user_profile/e84f9097828ae420d3f6578c742ab821a27d643b.png') }}"
-                        alt="">
-                </div>
-                <div class="flex items-center justify-between mt-10">
-                    <div class="">
-                        <span class="text-base-600 font-semibold">
-                            {{ __('PEN175.27') }}
-                        </span>
-                    </div>
-                    <div class="flex
-                                        items-center gap-2">
-                        <flux:icon name="clock" class="w-6 h-6 50" />
-                        <span class="text-xs-400 50">
-                            {{ __('Instant') }}
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <div class="bg-bg-secondary rounded-lg p-5">
-                <div class="flex items-center justify-between gap-4">
-                    <div class="flex items-center gap-2">
-                        <img src="{{ asset('assets/images/user_profile/frame.png') }}" alt="">
-                        <span class="text-[#4ADE80] text-base-400">{{ __('Xbox') }}</span>
-                    </div>
-                    <div class="flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                            class="w-10 h-10 text-text-secondary">
-                            <path d="M12 10.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3Z" />
-                        </svg>
-                        <span class="text-text-secondary text-base-600">{{ __('Stacked') }}</span>
-                    </div>
-                </div>
-                <div class="flex items-center mt-5">
-                    <span class="text-base-400">
-                        {{ __('Blue Squire Skin – 50 VB – Xbox / PSN / PC Full Access') }}
-                    </span>
-                    <img src="{{ asset('assets/images/user_profile/e84f9097828ae420d3f6578c742ab821a27d643b.png') }}"
-                        alt="">
-                </div>
-                <div class="flex items-center justify-between mt-10">
-                    <div class="">
-                        <span class="text-base-600 font-semibold ">
-                            {{ __('PEN175.27') }}
-                        </span>
-                    </div>
-                    <div class="flex
-                                        items-center gap-2">
-                        <flux:icon name="clock" class="w-6 h-6 50" />
-                        <span class="text-xs-400 50">
-                            {{ __('Instant') }}
-                        </span>
-                    </div>
-                </div>
-            </div>
+            
+
+              @foreach ($products as $product)
+              <x-ui.shop-card 
+                    :gameSlug="$product->games->slug"
+                    :categorySlug="$activeTab"
+                    :data="$product"
+                    :game="$product->games"
+                />
+               @endforeach
         </div>
+        @else 
+
+
+
+        @endif
 
         <div class="flex justify-end items-center space-x-3  p-4 m-10">
-            <button class="text-text-primary text-sm hover:text-zinc-500">{{ __('Previous') }}</button>
-
-            <button class="bg-zinc-600 text-white text-sm px-3 py-1 rounded">1</button>
-            <button class="text-text-primary text-sm hover:text-zinc-500">2</button>
-            <button class="text-text-primary text-sm hover:text-zinc-500">3</button>
-            <button class="text-text-primary text-sm hover:text-zinc-500">4</button>
-            <button class="text-text-primary text-sm hover:text-zinc-500">5</button>
-
-            <button class="text-text-primary text-sm hover:text-zinc-500">{{ __('Next') }}</button>
+            
+            <x-frontend.pagination-ui :pagination="$pagination" />
         </div>
-    @endif
 </main>
