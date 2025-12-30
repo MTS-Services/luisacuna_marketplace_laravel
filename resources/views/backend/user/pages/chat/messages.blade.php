f95bbaa305d221efa3bd8e639b68a6643be52bf7
dev-sumon <akhtaruzzamansumon7@gmail.com>
2025-12-30 17:13:39 +0600

    user dashboard message some update

--- resources/views/backend/user/pages/chat/messages.blade.php
+++ resources/views/backend/user/pages/chat/messages.blade.php
@@ -1,5 +1,6 @@
 <x-user::app>
      <x-slot name="pageSlug">{{ __('messages') }}
      </x-slot>
-    <livewire:backend.user.chat.index/>
+    {{-- <livewire:backend.user.chat.index/> --}}
+    <livewire:backend.user.chat.index-user />
 </x-user::app>
