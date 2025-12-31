e9fee5cbc63e71d21c626ee8bc9015b54cef0567
dev-sumon <akhtaruzzamansumon7@gmail.com>
2025-12-31 17:44:38 +0600

    All Transactions, Purchased, Top Ups, Withdrawals  index page ready

---
+++ app/Livewire/Backend/Admin/FinanceManagement/AllTransactions.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Livewire\Backend\Admin\FinanceManagement;
+
+use Livewire\Component;
+use App\Services\TransactionService;
+use App\Traits\Livewire\WithDataTable;
+use App\Traits\Livewire\WithNotification;
+
+class AllTransactions extends Component
+{
+    use WithDataTable, WithNotification;
+
+    public $statusFilter = '';
+    public $showDeleteModal = false;
+    public $deleteCategoryId = null;
+    public $bulkAction = '';
+    public $showBulkActionModal = false;
+    public $deleteId = null;
+
+    protected TransactionService $service;
+
+    public function boot(TransactionService $service)
+    {
+        $this->service = $service;
+    }
+    public function render()
+    {
+        $datas = $this->service->paginateDatas(
+            perPage: $this->perPage,
+            filters: $this->getFilters()
+        );
+
+
+        $columns = [
+            [
+                'key' => 'transaction_id',
+                'label' => 'Transaction ID',
+                'sortable' => true,
+            ],
+            [
+                'key' => 'user_id',
+                'label' => 'User',
+                'sortable' => true,
+                'format' => fn($data) => '<a href="' . route('profile', ['username' => $data->user->username]) . '" target="_blank"><span class="text-zinc-500 text-xs xxs:text-sm md:text-base truncate">' . $data->user->full_name . '</span></a>'
+            ],
+            [
+                'key' => 'amount',
+                'label' => 'Price ($)',
+                'sortable' => true,
+                'format' => fn($data) => '<span class="font-semibold ' . $data->calculation_type->textColor() . ' ">' . $data->calculation_type->prefix() . ' ' . $data->net_amount ?? 0 . '</span>'
+            ],
+            [
+                'key' => 'payment_gateway',
+                'label' => 'Payment Method',
+                'sortable' => true,
+                'format' => fn($data) => $data->payment_gateway,
+            ],
+            [
+                'key' => 'created_at',
+                'label' => 'Created',
+                'sortable' => true,
+                'format' => function ($data) {
+                    return $data->created_at_formatted;
+                }
+            ],
+        ];
+        $actions = [
+            [
+                'key' => 'order_id',
+                'label' => 'View',
+                'x_click' => "\$dispatch('order-detail-modal-open', { orderId: '{value}' }); console.log('open');",
+
+            ],
+        ];
+        $bulkActions = [
+            ['value' => 'delete', 'label' => 'Delete'],
+
+        ];
+        return view('livewire.backend.admin.finance-management.all-transactions', [
+            'datas' => $datas,
+            'columns' => $columns,
+            'actions' => $actions,
+            'bulkActions' => $bulkActions,
+        ]);
+    }
+
+    public function resetFilters(): void
+    {
+        $this->reset(['search', 'statusFilter', 'perPage', 'sortField', 'sortDirection', 'selectedIds', 'selectAll', 'bulkAction']);
+        $this->resetPage();
+    }
+
+    protected function getFilters(): array
+    {
+        return [
+            // 'status'         => $this->statusFilter,
+            'sort_field'     => $this->sortField,
+            'sort_direction' => $this->sortDirection,
+        ];
+    }
+
+
+    public function updatedStatusFilter(): void
+    {
+        $this->resetPage();
+    }
+}
--- resources/views/backend/admin/pages/order-management/order.blade.php
+++ resources/views/backend/admin/pages/order-management/order.blade.php
@@ -1,32 +1,3 @@
-{{-- <x-admin::app>
-    <x-slot name="pageSlug">{{ __('order-management') }}</x-slot>
-    @switch(Route::currentRouteName())
-        @case('admin.orders.paid-orders')
-            <x-slot name="pageSlug">{{ __('paid-orders') }}</x-slot>
-            <x-slot name="title">{{ __('Paid Orders') }}</x-slot>
-            <x-slot name="breadcrumb">{{ __('Order Management / Paid Orders List') }}</x-slot>
-            <livewire:backend.admin.order-management.paid-orders />
-        @break
-        @case('admin.orders.completed-orders')
-            <x-slot name="pageSlug">{{ __('completed-orders') }}</x-slot>
-            <x-slot name="title">{{ __('Complete Orders') }}</x-slot>
-            <x-slot name="breadcrumb">{{ __('Order Management / Complete Orders List') }}</x-slot>
-            <livewire:backend.admin.order-management.complete-orders />
-        @break
-        @case('admin.orders.cancelled-orders')
-            <x-slot name="pageSlug">{{ __('cancelled-orders') }}</x-slot>
-            <x-slot name="title">{{ __('Cancelled Orders') }}</x-slot>
-            <x-slot name="breadcrumb">{{ __('Order Management / Cancelled Orders List') }}</x-slot>
-            <livewire:backend.admin.order-management.cancelled-orders />
-        @break
-
-        @default
-            <x-slot name="title">{{ __('All Orders') }}</x-slot>
-            <x-slot name="breadcrumb">{{ __('Order Management / All Orders List') }}</x-slot>
-            <livewire:backend.admin.order-management.all-orders />
-    @endswitch
-</x-admin::app> --}}
-
 
 <x-admin::app>
     <x-slot name="pageSlug">{{ __('order-management') }}</x-slot>
