517eaad767b8763f88e7b279e6e5240b9d215527
Md Monirul Islam Taluqdar <82707883+monirulislam0@users.noreply.github.com>
2026-01-06 14:42:48 +0600

    updated

--- app/Helpers/game_helpers.php
+++ app/Helpers/game_helpers.php
@@ -9,3 +9,23 @@ if (!function_exists('delivery_methods')) {
         ];
     }
 }
+
+if (!function_exists('delivery_timelines')) {
+    function delivery_timelines($deliveryMethod = null)
+    {
+        if($deliveryMethod == 'manual') {
+            return [
+                'instant' => 'Instant Delivery',
+                '1-hour' => '1 Hour',
+                '12-hour' => '12 Hours',
+                '24-hour' => '24 Hours',
+                '1-day' => '1 Day'
+            ];
+        }else{
+           return [
+                'instant' => 'Instant Delivery',
+            ]; 
+        }
+    }
+}
+
