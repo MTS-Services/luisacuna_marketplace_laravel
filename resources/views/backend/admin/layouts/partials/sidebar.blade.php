55825e84a06d09d762360d6018c00358689451bb
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2026-02-25 12:40:18 +0600

    Update sidebar.blade.php

--- resources/views/backend/admin/layouts/partials/sidebar.blade.php
+++ resources/views/backend/admin/layouts/partials/sidebar.blade.php
@@ -186,7 +186,7 @@
                             'name' => $cat->name,
                             'route' => route('admin.pm.category.index', $cat->slug),
                             // 'icon' => $cat->icon ?? 'circle',
-                            'img' => storage_url($cat->icon),
+                            'img' => $cat->icon ? storage_url($cat->icon) : null,
                             'active' => $cat->slug,
                         ];
                     }
